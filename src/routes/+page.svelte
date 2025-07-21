940a08a39f5d29dbebbf964e187736176fb1f581
forewit <marcda495@gmail.com>
2025-07-21 14:43:50 -0400

    na

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import DamageThresholds from '$lib/daggerbrain/components/character sheet/DamageThresholds.svelte';
 	import EvasionSlot from '$lib/daggerbrain/components/character sheet/EvasionSlot.svelte';
 	import HopeTracker from '$lib/daggerbrain/components/character sheet/HopeSlot.svelte';
+	import Inventory from '$lib/daggerbrain/components/character sheet/Inventory.svelte';
 	import Level from '$lib/daggerbrain/components/character sheet/Level.svelte';
 	import Traits from '$lib/daggerbrain/components/character sheet/Traits.svelte';
 </script>
@@ -15,3 +16,4 @@
 <Traits class="border" />
 <Level class="border" />
 <DamageThresholds class="border" />
+<Inventory class="border" />
